aee0b087e84d085aa237a3c000cd7b06b207db7d
RanjithaSabai007 <ranjithasabai007@gmail.com>
2026-03-30 09:10:12 +0530

    Added UC8

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,119 +1,86 @@
-import java.util.HashMap;
+import java.util.*;
 
-abstract class Room {
-    private String name;
-    private int numberOfBeds;
-    private double size;
-    private double price;
+class Reservation {
+    private String reservationId;
+    private String guestName;
+    private String roomType;
 
-    public Room(String name, int numberOfBeds, double size, double price) {
-        this.name = name;
-        this.numberOfBeds = numberOfBeds;
-        this.size = size;
-        this.price = price;
+    public Reservation(String reservationId, String guestName, String roomType) {
+        this.reservationId = reservationId;
+        this.guestName = guestName;
+        this.roomType = roomType;
     }
 
-    public String getName() {
-        return name;
+    public String getReservationId() {
+        return reservationId;
     }
 
-    public int getNumberOfBeds() {
-        return numberOfBeds;
+    public String getGuestName() {
+        return guestName;
     }
 
-    public double getSize() {
-        return size;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public abstract void displayDetails();
-}
-
-class SingleRoom extends Room {
-    public SingleRoom() {
-        super("Single Room", 1, 200.0, 100.0);
-    }
-
-    @Override
-    public void displayDetails() {
-        System.out.println(getName() + " - Beds: " + getNumberOfBeds() + ", Size: " + getSize() + " sq ft, Price: $" + getPrice());
+    public String getRoomType() {
+        return roomType;
     }
 }
 
-class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super("Double Room", 2, 350.0, 180.0);
+class BookingHistory {
+    private List<Reservation> history;
+
+    public BookingHistory() {
+        history = new ArrayList<>();
     }
 
-    @Override
-    public void displayDetails() {
-        System.out.println(getName() + " - Beds: " + getNumberOfBeds() + ", Size: " + getSize() + " sq ft, Price: $" + getPrice());
-    }
-}
-
-class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super("Suite Room", 3, 600.0, 400.0);
+    public void addReservation(Reservation reservation) {
+        history.add(reservation);
     }
 
-    @Override
-    public void displayDetails() {
-        System.out.println(getName() + " - Beds: " + getNumberOfBeds() + ", Size: " + getSize() + " sq ft, Price: $" + getPrice());
+    public List<Reservation> getAllReservations() {
+        return Collections.unmodifiableList(history);
     }
 }
 
-class RoomInventory {
-    private HashMap<String, Integer> inventory;
+class BookingReportService {
+    private BookingHistory bookingHistory;
 
-    public RoomInventory() {
-        inventory = new HashMap<>();
+    public BookingReportService(BookingHistory bookingHistory) {
+        this.bookingHistory = bookingHistory;
     }
 
-    public void addRoomType(String roomType, int availability) {
-        inventory.put(roomType, availability);
-    }
-
-    public int getAvailability(String roomType) {
-        return inventory.getOrDefault(roomType, 0);
-    }
-
-    public void updateAvailability(String roomType, int newAvailability) {
-        if (inventory.containsKey(roomType)) {
-            inventory.put(roomType, newAvailability);
+    public void printAllBookings() {
+        List<Reservation> list = bookingHistory.getAllReservations();
+        for (Reservation r : list) {
+            System.out.println(r.getReservationId() + " | " + r.getGuestName() + " | " + r.getRoomType());
         }
     }
 
-    public void displayInventory() {
-        System.out.println("Current Inventory:");
-        for (String roomType : inventory.keySet()) {
-            System.out.println(roomType + " - Availability: " + inventory.get(roomType));
+    public void printSummary() {
+        Map<String, Integer> countMap = new HashMap<>();
+        List<Reservation> list = bookingHistory.getAllReservations();
+
+        for (Reservation r : list) {
+            String type = r.getRoomType();
+            countMap.put(type, countMap.getOrDefault(type, 0) + 1);
+        }
+
+        for (String type : countMap.keySet()) {
+            System.out.println(type + " -> " + countMap.get(type));
         }
     }
 }
 
 public class BookMyStayApp {
     public static void main(String[] args) {
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        BookingHistory history = new BookingHistory();
 
-        RoomInventory inventory = new RoomInventory();
-        inventory.addRoomType(single.getName(), 5);
-        inventory.addRoomType(doubleRoom.getName(), 3);
-        inventory.addRoomType(suite.getName(), 2);
+        history.addReservation(new Reservation("R001", "Alice", "Single"));
+        history.addReservation(new Reservation("R002", "Bob", "Suite"));
+        history.addReservation(new Reservation("R003", "Charlie", "Single"));
 
-        single.displayDetails();
-        System.out.println("Availability: " + inventory.getAvailability(single.getName()));
+        BookingReportService reportService = new BookingReportService(history);
 
-        doubleRoom.displayDetails();
-        System.out.println("Availability: " + inventory.getAvailability(doubleRoom.getName()));
-
-        suite.displayDetails();
-        System.out.println("Availability: " + inventory.getAvailability(suite.getName()));
-
-        inventory.displayInventory();
+        reportService.printAllBookings();
+        System.out.println("Summary:");
+        reportService.printSummary();
     }
 }
